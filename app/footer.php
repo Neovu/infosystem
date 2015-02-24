0be44d717288b2eaff9d229b0de4638daa0412d7
schmaedech <schmaedech@gmail.com>
2015-02-24 20:34:47 -0300

    index

---
+++ app/footer.php
@@ -0,0 +1,5 @@
+  
+<div class="footer-cool">
+    <h4>&copy; Infosystem 2015. (48) 3344-0200 | 9158-1930 </h4>
+    <h4><a href="http://www.neovu.com.br" target="blank">by neovu</a></h4>
+</div>
--- app/index.php
+++ app/index.php
@@ -30,10 +30,7 @@
             </div>
         </form>
     </div>
-    <div class="footer-cool">
-        <h4>&copy; Infosystem 2015. (48) 3344-0200 | 9158-1930 </h4>
-        <h4><a href="http://www.neovu.com.br" target="blank">by neovu</a></h4>
-    </div>
+   
 </div>
 
-
+<div class="clear"></div>
