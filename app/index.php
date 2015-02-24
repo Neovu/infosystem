3156b91dda359693d469c273a593679381a37720
soliz0 <soliz0@gmail.com>
2015-02-24 17:31:11 -0300

    alteraceos diversas

--- app/index.php
+++ app/index.php
@@ -1,24 +1,48 @@
- 
-<form name = "meu_form"> 
-    <h1>Entre em contato</h1> 
-    <p class = "nome"> 
-        <input type = "text" id = "nomeid" placeholder = "" required = "required" name = "nome" /> 
-        <label for = "nome">Nome</label> 
-    </p> 
-    <p class = "fone">
-        <input type = "text" id = "foneid" placeholder = "(xx)xx-xx-xx-xx" name = "fone" /> 
-        <label for = "fone">Fone</label> 
-    </p> 
-    <p>
-        <input type = "email" id = "emailid" placeholder = "" name = "email" /> 
-        <label for = "email">Email</label> 
-    </p> 
-    <p> 
-        <textarea placeholder = "Deixe sua opnião">
-            
-        </textarea> 
-    </p> 
-    <p class = "submit"> 
-        <input type = "submit" onclick = "Enviar();" value = "Enviar" />
-    </p> 
-</form>
+<section class="container"> 
+
+    <div class="row">
+        <div class="center">
+            <h1 class="vert-offset-bottom-20"><img src="/templates/gantry/images/logo/light/logo.png"/></h1> 
+            <h2 class="vert-offset-bottom-10">Algo <span class="destaque">incrível</span> está para acontecer</h2> 
+        </div>
+        <form name = "meu_form" class="center-comp span5 vert-offset-bottom-40"> 
+
+            <div class="control-group">
+                <input type = "text" id = "nomeid" required = "required" name = "nome" placeholder="seu nome"class="span5 input-cool"/> 
+                <label for = "nome" class="hide">Nome</label> 
+            </div>
+
+
+            <div class="control-group">
+                <input type = "text" id = "foneid"  name = "fone" placeholder="seu telefone" class="span5 input-cool"/> 
+                <label for = "fone" class="hide">Fone</label> 
+            </div>
+
+            <div class="control-group">
+                <input type = "email" id = "emailid"  name = "email" placeholder="seu email" class="span5 input-cool"/> 
+                <label for = "email" class="hide">Email</label> 
+            </div>
+            <div class="control-group">
+                <textarea placeholder="sua mensagem" class="span5 input-cool"></textarea> 
+            </div>
+            <div class="control-group">
+                <input type = "submit" onclick = "Enviar();" value = "Enviar" class="btn-orange-cool btn-warning span-5"/>
+            </div>
+        </form>
+    </div>
+    <section class="social row">
+        <div class="span2 center-comp">
+            <a href="/templates/gantry/images/social/bt_face.png"><img src="/templates/gantry/images/social/bt_face.png"/></a>
+            <a href="/templates/gantry/images/social/bt_twitter.png"><img src="/templates/gantry/images/social/bt_twitter.png"/></a>   
+            <a href="/templates/gantry/images/social/bt_linkedin.png"><img src="/templates/gantry/images/social/bt_linkedin.png"/></a>
+            <a href="/templates/gantry/images/social/bt_google.png"><img src="/templates/gantry/images/social/bt_google.png"/></a>
+        </div>
+    </section>
+<section class="row center">
+        <h4>&copy; Infosystem 2015. (48) 3344-0200 | 9158-1930 </h4>
+        <h4><a href="http://www.neovu.com.br" target="blank">by neovu</a></h4>
+    </section>
+</section>
+
+
+
