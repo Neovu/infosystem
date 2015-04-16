0d9d75fd337d8c9f0563e839f06300447e21ceff
schmaedech <schmaedech@gmail.com>
2015-04-15 21:20:32 -0300

    chat

--- app/index.php
+++ app/index.php
@@ -5,14 +5,13 @@
             <h1 class="vert-offset-bottom-20"><img src="/templates/gantry/images/logo/light/logo.png"/></h1> 
             <h2 class="vert-offset-bottom-10">Algo <span class="destaque">incrível</span> está para acontecer</h2> 
         </div>
-        <form name = "meu_form" class="center-comp span5 vert-offset-bottom-40"> 
+        <div class="center">
+            <form name = "contact_form" class="center-comp span5 vert-offset-bottom-40 contact_form"> 
 
             <div class="control-group">
                 <input type = "text" id = "nomeid" required = "required" name = "nome" placeholder="seu nome"class="span5 input-cool"/> 
                 <label for = "nome" class="hide">Nome</label> 
-            </div>
-
-
+            </div> 
             <div class="control-group">
                 <input type = "text" id = "foneid"  name = "fone" placeholder="seu telefone" class="span5 input-cool"/> 
                 <label for = "fone" class="hide">Fone</label> 
@@ -26,15 +25,17 @@
                 <textarea placeholder="sua mensagem" class="span5 input-cool"></textarea> 
             </div>
             <div class="control-group">
-                <input type = "submit" onclick = "Enviar();" value = "Enviar" class="btn-orange-cool btn-warning span-5"/>
+                <input type = "submit" onclick = "Enviar();" value = "Enviar" class="btn-orange-cool btn-warning span5 input-cool"/>
             </div>
         </form>
+        </div>
+        
     </div>
 
 </div>
 
 <section class="social row">
-    <div class="span2 center-comp">
+    <div class="span2 center">
         <a href="/templates/gantry/images/social/bt_face.png"><img src="/templates/gantry/images/social/bt_face.png"/></a>
         <a href="/templates/gantry/images/social/bt_twitter.png"><img src="/templates/gantry/images/social/bt_twitter.png"/></a>   
         <a href="/templates/gantry/images/social/bt_linkedin.png"><img src="/templates/gantry/images/social/bt_linkedin.png"/></a>
