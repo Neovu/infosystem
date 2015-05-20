349b6ce6a441bcf4af613049e0c7ab3f2160ecd0
soliz0 <soliz0@gmail.com>
2015-05-20 19:15:05 -0300

    banner savapp

--- app/index.php
+++ app/index.php
@@ -1,3 +1,141 @@
+<div class="row-fluid">
+    <div class="container">
+        <div class="span6">
+            <h1>Call to Action</h1>
+            <p></p>
+        </div>
+        <div class="span6">
+            <img src="#"/>
+        </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="container">
+        <h2>No que acreditamos</h2>
+        <p>Lorem ipsum, blsit amet</p>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+        </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="container">
+        <h2>Nossos principais serviços</h2>
+        <p>Lorem ipsum, blsit amet</p>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Internet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Internet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Solucoes Completas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+            <p>
+                <a class="btn btn-default btn-lg" href="#"></a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="container">
+        <h2>Veja quem está comentando</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+        <div>
+            INSERIR CAROUSEL
+        </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span2">
+        <img src=""/>
+    </div>
+    <div class="span2">
+        <img src=""/>
+    </div>
+    <div class="span2">
+        <img src=""/>
+    </div>
+    <div class="span2">
+        <img src=""/>
+    </div>
+    <div class="span2">
+        <img src=""/>
+    </div>
+    <div class="span2">
+        <img src=""/>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="container">
+        <h2>Saiba mais sobre a gente</h2>
+        <p>Lorem ipsum, blsit amet</p>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Facebook</h2>
+            
+        </div>
+        <div class=span4">
+            <img src="#"/>
+            <h2>Twitter</h2>
+           
+        </div>
+        <div class="span4">
+            <img src="#"/>
+            <h2>Google +</h2>
+           
+        </div>
+    </div>
+</div>
+
+
 <div class="center">
 
     <h2 class="vert-offset-bottom-10">Algo <span class="destaque">incrível</span> está para acontecer</h2> 
