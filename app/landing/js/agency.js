8aaef590c483b9a852b412f05d5ad098b93022ea
soliz0 <soliz0@gmail.com>
2015-05-31 10:07:48 -0300

    Suporte, banner, valores/qualidades, clientes, testimonials, comecando planos de itnernet

--- app/landing/js/agency.js
+++ app/landing/js/agency.js
@@ -18,9 +18,15 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+$(function() {
+    $("img.lazy").lazyload({threshold: 80});
+    $("img.lazy").lazyload({effect: "fadeIn"});
+
 });
