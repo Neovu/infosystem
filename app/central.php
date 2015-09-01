1b7226d06e20df1605bd4c9061a529f25da5e126
schmaedech <schmaedech@gmail.com>
2015-08-31 22:58:04 -0300

    links

--- app/central.php
+++ app/central.php
@@ -16,14 +16,37 @@ if ($user->guest) {
 
     function loaded( ) {
 
-        jQuery('#central_admin').css("height", jQuery('iframe').contents().height());
+        // jQuery('#central_admin').css("height", jQuery('iframe').contents().height());
+        jQuery('#central_admin').css("height", "1200px");
         console.log(jQuery("body").height())
         console.log(jQuery('iframe').contents().height())
 
-    }
 
-</script> 
-<div id="central_admin" >
-    <iframe onload="loaded()" class="central_admin" src="http://central.maisinternet.net.br/central/" /> 
+
+    }
+    function loadlink(link) {
+
+        var links = [
+            'http://central.maisinternet.net.br/central/index.php?section=segundavia',
+            'http://painel.maisinternet.net.br/index.php?section=historico',
+            'http://painel.maisinternet.net.br/index.php?section=meusplanos',
+            'http://central.maisinternet.net.br/central/templates/medidor/initialmeter.php'];
+
+        jQuery("#central_iframe").attr('src', links[link]);
+
+    }
+</script>  
+
+
+<div class="container central">
+    <ul class="gf-menu gf-splitmenu">
+        <li class="item"><a href="#" onclick="loadlink(0)"> 2 via boleto</a></li>
+        <li class="item"><a href="#" onclick="loadlink(1)">extrato financeiro</a></li>
+        <li class="item"><a href="#" onclick="loadlink(2)"> dados do meu plano</a></li> 
+        <li class="item"><a href="#" onclick="loadlink(3)">medidor de velocidade</a></li>
+    </ul> 
+</div> 
+
+<div id="central_admin"> 
+    <iframe id="central_iframe" onload="loaded()" class="central_admin" src="http://central.maisinternet.net.br/central/" /> 
 </div>
- 
